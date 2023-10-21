a1db4aaf8bef66b978bac22d216acc5f7959c426
Hemant Antony <88136054+HemantAntony@users.noreply.github.com>
2023-10-21 22:37:38 +0530

    Removed comment

--- Assets/Chess/Scripts/Core/ChessItem.cs
+++ Assets/Chess/Scripts/Core/ChessItem.cs
@@ -133,7 +133,7 @@ public abstract class ChessItem
         _attackMoves.Add(new int[] { row, col });
     }
 
-    protected void AddLegalPositions(Vector2Int direction) // Add documentation
+    protected void AddLegalPositions(Vector2Int direction)
     {
         for (int row = _row + direction.x, col = _col + direction.y; row >= 0 && row <= 7 && col >= 0 && col <= 7; row += direction.x, col += direction.y)
         {
